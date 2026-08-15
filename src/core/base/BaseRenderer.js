1f61c8ffd7cc08840f0a9c3fe4f3966c004be0e3
Duong <duong@example.com>
2026-08-16 00:22:55 +0700

    refactor: simplify base renderer

--- src/core/base/BaseRenderer.js
+++ src/core/base/BaseRenderer.js
@@ -1,11 +1,14 @@
 export class BaseRenderer {
   constructor(containerId) {
+    this.containerId = containerId;
     this.container = document.getElementById(containerId);
     this._retryCount = 0;
     this._maxRetries = 2;
   }
 
   findContainer() {
+    this.container = document.getElementById(this.containerId) || this.container;
+
     if (!this.container && this._retryCount < this._maxRetries) {
       this._retryCount++;
       setTimeout(() => this.findContainer(), 100);
@@ -14,11 +17,8 @@ export class BaseRenderer {
   }
 
   render(data) {}
-
   renderEmpty() {}
-
   update(data) {}
-
   clear() {
     if (this.container) {
       this.container.innerHTML = '';
