838dea8ff2ceb05f29878df6a8ec56e10db599f1
Duong <duong@example.com>
2026-08-16 02:11:23 +0700

    overage 80

--- vite.config.js
+++ vite.config.js
@@ -32,10 +32,10 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'html', 'json'],
       thresholds: {
-        statements: 90,
-        branches: 90,
-        functions: 90,
-        lines: 90,
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
       },
       coverage: {
         exclude: [
