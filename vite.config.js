a3d07b8264b8988c69b8d2eefc012367c09dfac6
Duong <duong@example.com>
2026-08-16 01:54:40 +0700

    giảm ngưỡng cho overage

--- vite.config.js
+++ vite.config.js
@@ -32,10 +32,10 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'html', 'json'],
       thresholds: {
-        statements: 95,
-        branches: 95,
-        functions: 95,
-        lines: 95,
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90,
       },
       coverage: {
         exclude: [
