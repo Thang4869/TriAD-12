174c720f27fdc7491e9c22119d2b91cc7ed065ff
Duong <duong@example.com>
2026-08-17 01:18:35 +0700

    refactor: update products.config

--- src/config/products.config.js
+++ src/config/products.config.js
@@ -1,46 +1,46 @@
-import { Logger } from '../core/services/Logger.js';
+import { Logger } from "../core/services/Logger.js";
 
 export const products = [
-    {
-        id: 1,
-        name: "TriAD Storage Container (1000ml/37oz)",
-        color: "White",
-        price: 150000,
-        image: "../images/21.jpg",
-        filter: ""
-    },
-    {
-        id: 2,
-        name: "TriAD Storage Container (400ml/13.5oz)",
-        color: "White",
-        price: 110000,
-        image: "../images/22.jpg",
-        filter: ""
-    },
-    {
-        id: 3,
-        name: "TriAD Storage Container (800ml/27oz)",
-        color: "White",
-        price: 130000,
-        image: "../images/23.jpg",
-        filter: ""
-    },
-    {
-        id: 4,
-        name: "TriAD Storage Container (400ml/13.5oz)",
-        color: "White",
-        price: 110000,
-        image: "../images/24.jpg",
-        filter: ""
-    },
-    {
-        id: 5,
-        name: "TriAD Storage Container (Combo TriAD)",
-        color: "White",
-        price: 350000,
-        image: "../images/25.jpg",
-        filter: ""
-    }
+  {
+    id: 1,
+    name: "TriAD Storage Container (1000ml/37oz)",
+    color: "White",
+    price: 150000,
+    image: "../images/21.jpg",
+    filter: "",
+  },
+  {
+    id: 2,
+    name: "TriAD Storage Container (400ml/13.5oz)",
+    color: "White",
+    price: 110000,
+    image: "../images/22.jpg",
+    filter: "",
+  },
+  {
+    id: 3,
+    name: "TriAD Storage Container (800ml/27oz)",
+    color: "White",
+    price: 130000,
+    image: "../images/23.jpg",
+    filter: "",
+  },
+  {
+    id: 4,
+    name: "TriAD Storage Container (400ml/13.5oz)",
+    color: "White",
+    price: 110000,
+    image: "../images/24.jpg",
+    filter: "",
+  },
+  {
+    id: 5,
+    name: "TriAD Storage Container (Combo TriAD)",
+    color: "White",
+    price: 350000,
+    image: "../images/25.jpg",
+    filter: "",
+  },
 ];
 
 window.products = products;
