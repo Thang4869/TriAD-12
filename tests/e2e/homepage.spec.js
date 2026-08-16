08598a8aa4e34bb95e8b232cd800afbf1de0e6b0
Duong <duong@example.com>
2026-08-17 01:19:03 +0700

    test: update homepage.spec

--- tests/e2e/homepage.spec.js
+++ tests/e2e/homepage.spec.js
@@ -1,17 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage should display hero, about, features', async ({ page }) => {
-  await page.goto('/');
+test("homepage should display hero, about, features", async ({ page }) => {
+  await page.goto("/");
 
-  await expect(page.locator('#home h1')).toBeVisible();
-  await expect(page.locator('#home img')).toBeVisible();
+  await expect(page.locator("#home h1")).toBeVisible();
+  await expect(page.locator("#home img")).toBeVisible();
 
-  await expect(page.locator('#about h3')).toHaveText(/About Us/i);
-  await expect(page.locator('#about img').first()).toBeVisible();
+  await expect(page.locator("#about h3")).toHaveText(/About Us/i);
+  await expect(page.locator("#about img").first()).toBeVisible();
 
   const featuresHeading = page.locator('h2:has-text("Outstanding advantages")');
   await expect(featuresHeading).toBeVisible();
 
-  const featureItems = page.locator('.grid .p-6');
+  const featureItems = page.locator(".grid .p-6");
   await expect(featureItems).toHaveCount(4);
 });
